f4e86157c13366143e4b482f6ce846f95dcb8c43
Juan diego <diiegooliveros@gmail.com>
2019-03-11 00:17:40 -0500

    Se hace logica para que en la carga inicial de base de datos traiga solo el ultimo registro de configuracion. Tambien se hace la logica para centralizar las inserciones a la tabla de configuracion en base de datos y se valida cuando el dia local es igual al dia de base de datos para que iguale los datos locales a los datos de  db y en cazo de ser diferentes nos guarda unos nuevos datos de configuracion en db

--- server/classes/ticket-control.js
+++ server/classes/ticket-control.js
@@ -5,40 +5,54 @@ class TicketControl {
     constructor() {
         this.ultimo = 0;
         this.hoy = new Date().getDate();
-        ConfigModel.findOne().exec((err, configDb) => {
-            if (err){
+        //consulta en base de datos la ultima configuracion se ticketes donde esta el ultimo ticket y su dia
+        ConfigModel.findOne({}, {}, {sort: {'_id': -1}}).exec((err, configDb) => {
+            if (err) {
                 console.log(err);
                 return;
             }
-            if (!configDb){
+            if (!configDb) {
                 this.reiniciarConteo();
             }
 
-            if (configDb.hoy==this.hoy){
-
-            } else{
-                console.log(this.hoy);
-                console.log(configDb.hoy)
+            if (configDb.hoy == this.hoy) {
+                this.ultimo = configDb.ultimo;
+            } else {
                 this.reiniciarConteo();
             }
 
         })
     }
 
-    reiniciarConteo(){
-        let configmodel=new ConfigModel({
-            ultimo:this.ultimo,
-            hoy:this.hoy
+    reiniciarConteo() {
+        this.ultimo = 0;
+        console.log("Se ha inicializado el sistema");
+        this.guardarDb();
+    }
+
+    siguienteTicket() {
+        this.ultimo += 1;
+        this.guardarDb();
+        return `Ticket ${this.ultimo}`;
+    }
+
+    guardarDb() {
+        let configmodel = new ConfigModel({
+            ultimo: this.ultimo,
+            hoy: this.hoy
         });
 
-        configmodel.save((err,configdb)=>{
-            if (err){console.log("Error guardando en database")}
-            else{console.log(configdb)}
+        configmodel.save((err, configdb) => {
+            if (err) {
+                console.log("Error guardando en database")
+            } else {
+                console.log(configdb)
+            }
         });
-        console.log("Se ha inicializado el sistema");
     }
 
 }
+
 
 module.exports = {
     TicketControl
